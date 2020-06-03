cdd8ab28127f35bb8c1582b6565760eb3e88315e
Jen Power <powe0309@algonquinlive.com>
2020-06-03 15:46:19 -0400

    Add first if statement and validate

--- js/main.js
+++ js/main.js
@@ -9,3 +9,7 @@ var adj = prompt('An adjective (beautiful, super, etc.)');
 var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n\n Enter the letter below:')
+
+if (story == 'a') {
+  document.write('After snacking on ' + adj + ' treats, the ' + col + ' bellied ' + cre + ' ' + verb + ' for hours.');
+}

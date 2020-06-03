85d828691c1413d79b40a69e09c02ea478dc870a
Jen Power <powe0309@algonquinlive.com>
2020-06-03 15:29:07 -0400

    Add javascript variables

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var col = prompt('A colour (orange, indigo, etc.)');
+
+var cre = prompt('A creature (dragon, moth, etc.)');
+
+var adj = prompt('An adjective (beautiful, super, etc.)');
+
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like?
+a) Snacking
+b) Escaping
+c) Winning
+
+Enter the letter below:')

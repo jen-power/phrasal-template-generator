d74dff003ae873cc08bb8d7505bdd16268277c4b
Jen Power <powe0309@algonquinlive.com>
2020-06-03 16:00:59 -0400

    Move var declarations into a function

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 alert('Let’s make a story together!');
 
-var col = prompt('A colour (orange, indigo, etc.)');
+var generate = function() {
 
-var cre = prompt('A creature (dragon, moth, etc.)');
+  var col = prompt('A colour (orange, indigo, etc.)');
 
-var adj = prompt('An adjective (beautiful, super, etc.)');
+  var cre = prompt('A creature (dragon, moth, etc.)');
 
-var verb = prompt('A past tense verb (ran, burped, etc.)');
+  var adj = prompt('An adjective (beautiful, super, etc.)');
 
-var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n\n Enter the letter below:')
+  var verb = prompt('A past tense verb (ran, burped, etc.)');
 
-if (story == 'a' || 'A') {
-  document.write('After snacking on ' + adj + ' treats, the ' + col + ' bellied ' + cre + ' ' + verb + ' for hours.');
-}
+  var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n\n Enter the letter below:');
 
-if (story == 'b' || 'B') {
-  document.write('Amelia ' + verb + ' through the ' + adj + ' ' + col + ' nebula escaping the space ' + cre + '.');
-}
+  if (story == 'a') {
+    document.write('After snacking on ' + adj + ' treats, the ' + col + ' bellied ' + cre + ' ' + verb + ' for hours.');
+  }
 
-if (story == 'c' || 'C') {
-  document.write('Jackson chose his ' + adj + ', ' + col + ' ' + cre + ' card and ' + verb + ' it to the table knowing he won.');
-}
+  if (story == 'b') {
+    document.write('Amelia ' + verb + ' through the ' + adj + ' ' + col + ' nebula escaping the space ' + cre + '.');
+  }
+
+  if (story == 'c') {
+    document.write('Jackson chose his ' + adj + ', ' + col + ' ' + cre + ' card and ' + verb + ' it to the table knowing he won.');
+  }
+};
+
+generate();

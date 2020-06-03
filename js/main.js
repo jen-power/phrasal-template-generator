2e8d616ced8ab77a66a361ec38d63889bf2d7a4e
Jen Power <powe0309@algonquinlive.com>
2020-06-03 15:53:52 -0400

    Add two other if statements

--- js/main.js
+++ js/main.js
@@ -10,6 +10,14 @@ var verb = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning \n\n Enter the letter below:')
 
-if (story == 'a') {
+if (story == 'a' || 'A') {
   document.write('After snacking on ' + adj + ' treats, the ' + col + ' bellied ' + cre + ' ' + verb + ' for hours.');
+}
+
+if (story == 'b' || 'B') {
+  document.write('Amelia ' + verb + ' through the ' + adj + ' ' + col + ' nebula escaping the space ' + cre + '.');
+}
+
+if (story == 'c' || 'C') {
+  document.write('Jackson chose his ' + adj + ', ' + col + ' ' + cre + ' card and ' + verb + ' it to the table knowing he won.');
 }
